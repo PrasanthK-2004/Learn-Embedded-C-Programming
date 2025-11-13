4cbe5b52ae1cb7e59ce2ca5460d1085641fef6b1
PrasanthK-2004 <prasanthbe04@gmail.com>
2025-11-13 14:11:51 +0530

    Update 1. LED Blink Using Switch.c

--- ARM7 _LPC2129/SWITCH/1. LED Blink Using Switch.c
+++ ARM7 _LPC2129/SWITCH/1. LED Blink Using Switch.c
@@ -1,20 +1,33 @@
-// LED Blink Using Switch (Direct Register Access)
-
 #include <LPC21xx.h>
 
-int main() 
-{
+
+
+int main() {
+
     // Configure P0.10 as output (LED)
-    IODIR0 = (1 << 10);
+
+    IODIR0 |= (1 << 10);
+
     
-    // Configure P0.0 as input (Switch)
-		IOPIN0 = 1;
-    while (1) 
-    {
-        if (IOPIN0 & (1))           // If switch is pressed
-          IOSET0 = (1 << 10);    // Turn ON LED
-          else
+
+    // Configure P0.14 as input (Switch)
+
+    IODIR0 &= ~(1 << 14);
+
+    
+
+    while (1) {
+
+        if ((IOPIN0 & (1 << 14))==0) {   // If switch is pressed
+
+            IOSET0 = (1 << 10);    // Turn ON LED
+
+        } else {
+
             IOCLR0 = (1 << 10);    // Turn OFF LED
-        
+
+        }
+
     }
+
 }
